70441adcc898ce03cd63dc2a1c73488eed2e6f84
ykhr7121rkhy <--->
2022-04-11 11:56:44 +0900

    systeminit add

--- main.cpp
+++ main.cpp
@@ -2,38 +2,47 @@ using namespace std;
 #include <iostream>
 #include <vector>
 
+#define DIMENSION 2
+#define MAX_DELAY_UNIT 100000
 
 class state_system{
 public:
     double _in;
     double _out;
-    vector<double> x_d;
-    vector<double> x;
-    vector<vector<double>> a;
-    vector<double> b;
-    vector<double> c;
+    vector<double> data(DIMENSION,0.0) x_d;
+    vector<double> data(DIMENSION,0.0) x;
+    vector<vector<double>> data(DIMENSION,data(DIMENSION,0.0)) a;
+    vector<double> data(DIMENSION,0.0) b;
+    vector<double> data(DIMENSION,0.0) c;
     double _t;
-    vector<vector<int>> flag_a;
-    vector<int> flag_b;
-    vector<int> flag_c;
+    vector<vector<int>> data(DIMENSION,data(DIMENSION,1)) flag_a;
+    vector<int> data(DIMENSION,1) flag_b;
+    vector<int> data(DIMENSION,1) flag_c;
     double dev_min;
-    vector<double> mudajikan_array;
+    vector<double> data(MAX_DELAY_UNIT,0.0) mudajikan_array;
     int array_write_position;
     int array_read_position;
     int delay_num;
-    int dimension_size = 2;
+    int dimension_size = DIMENSION;
     int elem_base = 300;
     int flag;
 
-    void state_system(){
-        this->a = {{0.0,0.0},{0.0,0.0}};
-        this->b = {{0.0,0.0}};
-        this->c = {{0.0,0.0}};
-    }
     void delay_array_init(int delay_num){
         this->delay_num = delay_num;
         this->array_read_position = 0;
         this->array_write_position = this->delay_num;
+    }
+    void system_init(void){
+        int i;
+        this->_in = 0.0;
+        this->_out = 0.0;
+        for(i = 0;i < DIMENSION; i++){
+            this->x_d[i] = 0.0;
+        }
+
+        for(i = 0;i < DIMENSION; i++){
+            this->x[i] = 0.0;
+        }
     }
 
 
